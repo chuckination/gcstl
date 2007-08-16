a87b743050819576349f04fd8e832cacb04246ae
chuck_s <chuck_s@b43ba59f-4933-0410-9cdd-b1228976ca12>
2007-08-16 02:16:05 +0000

    Added copyright notice to cstl_set.c and cstl_map.c Added barebones automake harness for stack and queue. Added empty header and implementation files for stack and queue. Added svn:ignore properties for stack and queue directories.
    git-svn-id: https://gcstl.svn.sourceforge.net/svnroot/gcstl/trunk@59 b43ba59f-4933-0410-9cdd-b1228976ca12

---
+++ src/queue/cstl_queue.h
@@ -0,0 +1,36 @@
+/* Copyright (C) 2007, Charles Stewart */
+
+/* This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  */
+
+/*******************************************************************************
+ *
+ * $URL: https://gcstl.svn.sourceforge.net/svnroot/gcstl/trunk/src/list/cstl_list.h $
+ * $Id: cstl_list.h 56 2007-07-31 05:10:01Z chuck_s $
+ *
+ ******************************************************************************/
+
+#ifndef CSTL_QUEUE_H
+#define CSTL_QUEUE_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
--- src/set/cstl_set.c
+++ src/set/cstl_set.c
@@ -1,3 +1,26 @@
+/* Copyright (C) 2007, Charles Stewart */
+
+/* This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  */
+
+/*******************************************************************************
+ *
+ * $URL: svn+ssh://hades/var/svn/cstl/trunk/src/rbtree/cstl_rbtree.h $
+ * $Id: cstl_rbtree.h 46 2007-07-22 00:11:23Z chucks $
+ *
+ ******************************************************************************/
+
 #include "cstl_set.h"
 #include "cstl_destroy.h"
 
